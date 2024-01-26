25358aa3d2b4361765ee46d53fd7bc191ce8c266
Jesse <3191955858@qq.com>
2024-01-26 22:56:51 +0800

    SGI 一，二级配置器的声明与部分实现

---
+++ src/2_2_6/include/defaultAllocTemplate.h
@@ -0,0 +1,137 @@
+#ifndef __DEFAULT_ALLOC_TEMPLATE_H_
+#define __DEFAULT_ALLOC_TEMPLATE_H_
+
+#include <cstddef>
+#include "./mallocAllocTemplate.h"
+
+#define SGI_METHODS true
+#define MY_METHODS false
+
+/*第二级 SGI 配置器 __DEFAULT_ALLOC_TEMPLATE_H_ 刨析*/
+
+/*
+    在 SGI 的分配器实现中，针对分配较小内存（通常是小于 128 字节）会带来很多内存碎片造成额外开销的问题，
+    设计了有别于第一级配置器的第二级配置器，它通常会申请一大块内存池，并维护与之对应的 free-list。
+    若下次还有相同的内存需求，直接从自由链表中划出，如果使用者释放内存块，就重新回收到自由链表中。
+
+    此外，为了方便管理，第二级配置器会主动将任何小额的内存需求量上调至 8 的倍数（比如 要申请 30 字节，就对齐到 32 字节），
+    并维护 16 个 free-list，分别对应 8，16，24，32，40，48，56，64，72，80，88，96，104，112，120，128 字节的小额区块。
+
+    由于使用联合体（union）的原因，维护链表不会因为需要额外的指针而浪费内存。
+*/
+
+enum {__ALIGN = 8};                             // 小型区块的上调边界              
+enum {__MAX_BYTES = 128};                       // 小型区块的上限
+enum {__NFPREELISTS = __MAX_BYTES / __ALIGN};   // free-list 数量
+
+/**
+ * @tparam Threads  用于多线程，当前不讨论这种情况
+ * @tparam Inst     可能用于后面的具体化，但是目前完全没排上用场
+*/
+template <bool Threads, int Inst>
+class __DefaultAllocTemplate
+{
+    private:
+        /**
+         * @brief           该函数将内存对齐至 8 的倍数
+         * 
+         * @param __bytes   传入的字节数
+         * 
+         * @return          对齐后的字节数
+        */
+        static std::size_t roundUp(std::size_t __bytes) 
+        { 
+            /*
+                这条语句拆解分析如下:
+                1. ((__bytes) + __ALIGN - 1)    在原始字节数的基础上加上 7，目的是为了离对齐边界更近一些
+                2. ~(__ALIGN - 1)               取反 0x00000007 的所有位 为 0xFFFFFFF8
+                3. 最后将 （1）的值和（2）的值进行与运算，得到比这个数对齐后的字节数（如 119 对齐到 120，98 对齐到 104）
+            */
+            return ((__bytes) + __ALIGN - 1) & ~(__ALIGN - 1); 
+        }
+
+    private:
+        union Obj           // free-list 节点的构成
+        {
+            union Obj * freeListLink;       // 指向下一个节点的指针
+            char clientData[1];             // 指向一个实际区块的指针
+        };
+    
+    private:
+        /*
+            16 个持有不同大小内存的自由链表，
+            考虑到多线程环境，使用 volatile 关键字避免编译器做出不正确的优化。
+        */
+        static Obj * volatile freeList[__NFPREELISTS];
+
+        /**
+         * @brief           通过传入的字节数，计算需要调用哪一个大小的链表（下标从 0 开始）
+         * 
+         * @param __bytes   传入的字节数
+         * 
+         * @return          返回需要调用第几号链表
+        */
+        static std::size_t freeListIndex(std::size_t __bytes) 
+        { 
+#if SGI_METHODS
+            /*
+                这条语句分析拆解如下：
+                1. (((__bytes) + __ALIGN - 1)       将传入的字节数和 7 相加，使其更接近对齐后的内存
+                2. ... / __ALIGN                    计算（1）得出的数据是 8 的 n 倍（小数点会被截断）
+                3. ... - 1                          由于下标从 0 开始，因此需要 - 1
+            */
+            return (((__bytes) + __ALIGN - 1) / __ALIGN - 1); 
+#elif MY_METHODS    
+
+            /**
+             * 而我的实现会直截了当一些，
+             * 直接调用 roundUp() 对齐内存，然后除以 8 再 - 1 得到下标（显然，在频繁调用下，没有 SGI 实现的有效率）
+            */
+            return roundUp(__bytes) / __ALIGN - 1;
+#endif
+        }
+
+        static void * reFill(std::size_t __n);
+
+        static void * chunkAlloc(std::size_t __size, int &nobJs);
+
+        static char * startFreeList;
+        static char * endFreeList;
+        static std::size_t heapSize;
+
+    public:
+        /**
+         * @brief 空间配置函数 allcate，传入需要配置的空间大小，
+         * 根据是否大于 128 bytes 来决定分配器的使用。
+         * 
+         * @brief 1. 大于 128 bytes，直接调用一级分配器，具体实现在 ./mallocAllocTemplate.h
+         * 
+         * @brief 2. 小于 128 bytes，检查对应的 free-list，
+         *           若链表内由可用的区块，就直接从表中拨出，若没有可用区块，就将区块大小上调至 8 倍数边界，
+         *           然后调用 reFill()，准备为 free-list 重新填充空间。
+         * 
+         * @param __n   要分配的内存大小
+         * 
+         * @return      分配完成后内存块的首地址
+        */
+        static void * allcate(std::size_t __n);
+
+        static void deallocate(void * __ptr, std::size_t __n);
+
+        static void * reallocate(void * __ptr, std::size_t oldSize, std::size_t newSize);
+};
+
+template <bool Threads, int Inst>
+char * __DefaultAllocTemplate<Threads, Inst>::startFreeList = nullptr;
+
+template <bool Threads, int Inst>
+char * __DefaultAllocTemplate<Threads, Inst>::endFreeList = nullptr;
+
+template <bool Threads, int Inst>
+std::size_t __DefaultAllocTemplate<Threads, Inst>::heapSize = 0;
+
+template <bool Threads, int Inst>
+typename __DefaultAllocTemplate<Threads, Inst>::Obj * volatile
+__DefaultAllocTemplate<Threads, Inst>::freeList[__NFPREELISTS] = {nullptr};
+
+#endif // __DEFAULT_ALLOC_TEMPLATE_H_
