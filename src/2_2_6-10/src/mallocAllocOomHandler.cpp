cb2968a9960071f46bce5948fe8bf7745e3ebfeb
Jesse <3191955858@qq.com>
2024-01-31 21:19:24 +0800

    在 Windows 平台下的一个简易 OOM 处理例程

---
+++ src/2_2_6-10/src/mallocAllocOomHandler.cpp
@@ -0,0 +1,31 @@
+#include "./include/mallocAllocOomHandler.h"
+
+static void ReleaseMemory()
+{
+    HANDLE hProcess = GetCurrentProcess();
+    PMEMORY_BASIC_INFORMATION pmem = nullptr;
+    PVOID hightestAddress = (PVOID)0x7FFFFFFFFFFF;
+    MEMORY_BASIC_INFORMATION mbi; 
+
+    while (VirtualQueryEx(hProcess, pmem, &mbi, sizeof(mbi)))
+    {
+        if ((mbi.State == MEM_COMMIT) && (mbi.RegionSize > BIG_MEMORY) && (mbi.Protect = PAGE_READWRITE))
+        {
+            VirtualFreeEx(hProcess, mbi.BaseAddress, mbi.RegionSize, MEM_DECOMMIT);
+            break;
+        }
+    }
+}
+
+static void myAllocHandler()
+{
+    void * result = VirtualAlloc(nullptr, BIG_MEMORY, MEM_COMMIT, PAGE_READWRITE);
+
+    if (result != nullptr) { return; }
+
+    ReleaseMemory();
+
+    result = VirtualAlloc(nullptr, BIG_MEMORY, MEM_COMMIT, PAGE_READWRITE);
+
+    ExitProcess(EXIT_FAILURE);
+}
