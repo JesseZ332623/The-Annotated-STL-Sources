49135b1360ac05bd6f6a4cd52492d560b35a85aa
Jesse <3191955858@qq.com>
2024-02-11 00:56:42 +0800

    链表节点的实现

--- src/3_2/myForwardList/src/include/List_Item.h
+++ src/3_2/myForwardList/src/include/List_Item.h
@@ -1,6 +1,8 @@
 #ifndef _LIST_ITEM_H_
 #define _LIST_ITEM_H_
 
+//#include <stdexcept>
+
 template <typename Type>
 class MyListItem
 {
@@ -13,7 +15,11 @@ class MyListItem
 
         Type getValue() const { return value; }
         
-        MyListItem * next() const { return nextItem; }
+        MyListItem * next() const 
+        { 
+            //if (!(nextItem->next())) { throw std::out_of_range("out of range! MyListItem * next() const {}"); }
+            return nextItem; 
+        }
 
         void setNext(MyListItem * __nextItem) { nextItem = __nextItem; } 
 };
