75c7c436debfbc3ae0184150fec0a5b5938b2ea0
Jesse <3191955858@qq.com>
2024-02-11 00:57:23 +0800

    单向链表迭代器的声明与实现

--- src/3_2/myForwardList/src/include/List_Iterator.h
+++ src/3_2/myForwardList/src/include/List_Iterator.h
@@ -3,12 +3,15 @@
 
 //#include "./List_Item.h"
 #include <iterator>
+#include <exception>
 
 template <typename Item>
 class ListIterator
 {
-    public:
+    private:
         Item * itemPointer;
+
+    public:
 
         //template<typename T>
         //struct iterator_traits<ListIterator<T>> { using iterator_category = std::forward_iterator_tag; };
@@ -16,11 +19,43 @@ class ListIterator
 
         ListIterator(Item * __itemPtr = nullptr) : itemPointer(__itemPtr) {}
 
-        Item & operator*() const { return *itemPointer; }
-        Item * operator->() const { return itemPointer; }
+        Item * get() const { return itemPointer; }
 
-        ListIterator & operator++() { itemPointer = itemPointer->next(); return *this; }
-        ListIterator operator++(int) { ListIterator tempIter = *this; ++(*this); return tempIter; }
+        Item & operator*() const 
+        { 
+            return *itemPointer; 
+        }
+        Item * operator->() const 
+        { 
+            return itemPointer; 
+        }
+
+        ListIterator & operator++() 
+        { 
+            itemPointer = itemPointer->next(); 
+            //if (!(itemPointer->next())) { throw std::out_of_range("Iterator out of range! operator++()\n"); }
+            return *this; 
+        }
+        ListIterator operator++(int) 
+        { 
+            ListIterator tempIter = *this; 
+            ++(*this); 
+            //if (!(itemPointer->next())) { throw std::out_of_range("Iterator out of range! operator++(int)\n"); }
+            return tempIter; 
+        }
+
+        ListIterator operator+(std::size_t __val) 
+        { 
+            if (__val < 0) { throw std::invalid_argument("Parameter val cannot be negative for forward iterator. operator+(std::size_t __val)"); }
+
+            for (std::size_t index = 0; index < __val; ++index) 
+            { 
+                itemPointer = itemPointer->next();
+                //if (!(itemPointer->next())) { throw std::out_of_range("Iterator out of range! operator+(std::size_t __val)"); }
+            }
+
+            return *this;
+        }
 
         bool operator==(const ListIterator & __iter) const { return (itemPointer == __iter.itemPointer); }
         bool operator!=(const ListIterator & __iter) const { return (itemPointer != __iter.itemPointer); }
