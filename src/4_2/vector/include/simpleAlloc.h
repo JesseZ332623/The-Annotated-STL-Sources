e07ea45eeffaa821de15a8fa13197c754bf3bbca
Jesse <3191955858@qq.com>
2024-06-18 00:07:41 +0800

    一个封装了分配器接口的类，实现基本的内存分配于释放

---
+++ src/4_2/vector/include/simpleAlloc.h
@@ -0,0 +1,35 @@
+#ifndef _SIMPLE_ALLOC_H_
+#define _SIMPLE_ALLOC_H_
+
+#include <cctype>
+
+template <typename Type, typename Alloc>
+class Simple_Alloc
+{
+    public:
+        static Type * allocate(std::size_t __n)
+        {
+            Alloc allocInstance;
+            return (!__n) ? nullptr : allocInstance.allocate(__n * sizeof(Type));
+        }
+
+        static Type * allocate(void)
+        {
+            Alloc allocInstance;
+            return (Type *)allocInstance.allocate(sizeof(Type));
+        }
+
+        static void deallocate(Type * __ptr, std::size_t __n)
+        {
+            Alloc allocInstance;
+            if (__n != 0) { allocInstance.deallocate(__ptr, __n * sizeof(Type)); }
+        }
+
+        static void deallocate(Type * __ptr) 
+        {
+            Alloc allocInstance;
+            allocInstance.deallocate(__ptr, sizeof(Type)); 
+        }
+};
+
+#endif // _SIMPLE_ALLOC_H_
