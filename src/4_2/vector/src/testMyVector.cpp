abc121f0ce970aa0d15c6802d9a0e4853c173acf
Jesse <3191955858@qq.com>
2024-06-19 18:39:26 +0800

    自己手搓的 vector 容器 测试用例

--- src/4_2/vector/src/testMyVector.cpp
+++ src/4_2/vector/src/testMyVector.cpp
@@ -3,9 +3,14 @@
 
 #include <iostream>
 #include <algorithm>
+#include <cstdlib>
+#include <ctime>
 
 template <typename Type>
 void showVector(const My_Vector<Type> & __vec, const char * __outputMsg);
+
+template <typename Type>
+void inputRandomData(My_Vector<Type> & __vec, const std::size_t __dataCount);
 
 int main(int argc, char const *argv[])
 {
@@ -13,13 +18,29 @@ int main(int argc, char const *argv[])
 
     system("cls");
 
+    /**
+     * 看似 vector 的实现如此冗长，
+     * 其实在栈上的只有 3 个 Type 类型的指针，共 24 字节。
+    */
+    CORRECT_LOG("Default My_Vector<int> size = " + std::to_string(sizeof(My_Vector<int>)) + " Bytes.\n");
+
     My_Vector<int> newVec(5, 1);
 
-    showVector(newVec, "Init My_Vector<int> container:\n");
+    My_Vector<double> vec_2 = {1, 2, 3, 4, 5, 8, 2456, 25, 0.56445};
 
-    newVec.push_back(2);
-    newVec.push_back(3);
-    newVec.push_back(4);
+    My_Vector<double> vec_3(vec_2);
+
+    showVector(newVec, "Init My_Vector<int> container newVec:\n");
+    showVector(vec_2, "Init My_Vector<double> container vec_2:\n");
+    showVector(vec_3, "Init My_Vector<double> container vec_3 copy by vec_2:\n");
+
+    My_Vector<int> vec_4 = std::move(newVec);
+
+    showVector(vec_4, "Init My_Vector<int> container moved by vec_4:\n");
+    showVector(newVec, "After move(), newVec: \n");
+
+#if true
+    inputRandomData(newVec, 15);
 
     showVector(newVec, "After push_back(const int & __n):\n");
 
@@ -43,6 +64,28 @@ int main(int argc, char const *argv[])
 
     showVector(newVec, "After insert(int *__pos, std::size_t __n, const int &__x):\n");
 
+    newVec[0] = 125;
+
+    showVector(newVec, "After operator[](sizeType __n):\n");
+
+    newVec.at(15) = 99;
+
+    showVector(newVec, "After at(sizeType __n):\n");
+
+    newVec.resize(25, 114);
+
+    showVector(newVec, "After resize(sizeType __newSize, const valueType & __x):\n");
+
+    newVec.resize(5);
+
+    showVector(newVec, "After resize(sizeType __newSize):\n");
+
+    My_Vector<int> vec_5;
+    vec_5.reserve(77);
+
+    showVector(vec_5, "vec_5 after reserve(sizeType __newSize):\n");
+#endif
+
     DONE
 
     return EXIT_SUCCESS;
@@ -57,12 +100,23 @@ void showVector(const My_Vector<Type> & __vec, const char * __outputMsg)
 
     printSplitLine(30, '-');
 
-    if (__outputMsg || *__outputMsg != '\0') { NOTIFY_LOG(__outputMsg); }
+    if (__outputMsg != nullptr) { NOTIFY_LOG(__outputMsg); }
 
-    CORRECT_LOG("vector size     = " + std::to_string(__vec.size()) + '\n');
-    CORRECT_LOG("vector capacity = " + std::to_string(__vec.capacity()) + '\n');
+    CORRECT_LOG("vector size     = [" + std::to_string(__vec.size()) + "]\n");
+    CORRECT_LOG("vector capacity = [" + std::to_string(__vec.capacity()) + "]\n");
 
     std::for_each(__vec.begin(), __vec.end(), outputRules); std::cout.put('\n');
 
     printSplitLine(30, '-');
+}
+
+template <typename Type>
+void inputRandomData(My_Vector<Type> & __vec, const std::size_t __dataCount)
+{
+    std::srand(std::time(nullptr));
+
+    for (std::size_t index = 0ULL; index < __dataCount; ++index)
+    {
+        __vec.push_back(std::rand() % 114 + 1);
+    }
 }
