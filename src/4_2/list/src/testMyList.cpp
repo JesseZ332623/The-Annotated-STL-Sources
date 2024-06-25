0871a5bdcfa44a3f804dcc254fc287ed1dbf73c9
Jesse <3191955858@qq.com>
2024-06-25 23:47:25 +0800

    双向链表 list 的测试用例

--- src/4_2/list/src/testMyList.cpp
+++ src/4_2/list/src/testMyList.cpp
@@ -1,8 +1,54 @@
 #include "../include/list.h"
 
+#include <MyLib/myLogerDef.h>
+#include <algorithm>
+
+template <typename Type>
+void showList(const MyList<Type> & __myList, const char __note);
+
 int main(int argc, char const *argv[])
 {
-    MyList<int> list_1;
+    system("cls");
+
+    MyList<std::string> stringList;
+
+    stringList.push_back("123123123");
+    stringList.push_back("This is a test string.");
+    stringList.push_back("This is a test string.");
+    stringList.push_back("Try one's best.");
+    stringList.push_back("This is a worse world.");
+    stringList.push_back("Why are you billy me??");
+    stringList.push_back("Do you like van you see~");
+    stringList.push_back("Oh my fucking god!");
+
+    showList(stringList, '\n');
+
+    stringList.unique();
+
+    showList(stringList, '\n');
 
     return EXIT_SUCCESS;
+}
+
+template <typename Type>
+void showList(const MyList<Type> & __myList, const char __note)
+{
+    using namespace MyLib::MyLoger;
+
+    printSplitLine(45, '-');
+
+    NOTIFY_LOG("List size = [" + std::to_string(__myList.size()) + "]\n");
+
+    auto tempIter = __myList.begin();
+
+    for (const Type & n : __myList) 
+    { 
+        ++tempIter;
+        CORRECT_LOG(n);
+
+        if (tempIter != __myList.end()) { std::cout.put('\n'); }
+    }
+
+    std::cout.put('\n'); 
+    printSplitLine(45, '-');
 }
