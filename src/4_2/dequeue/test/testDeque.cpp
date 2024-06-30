a080bdf7764dfd769b7b27be8194e143a18cfe6f
Jesse <3191955858@qq.com>
2024-06-30 22:22:04 +0800

    双端队列容器的测试用例

---
+++ src/4_2/dequeue/test/testDeque.cpp
@@ -0,0 +1,12 @@
+#include "../include/deque.h"
+
+#include <deque>
+
+#include <MyLib/myLogerDef.h>
+
+int main(int argc, char const *argv[])
+{
+    Deque_Iterator<int, int &, int *, 0> iter;
+
+    return EXIT_SUCCESS;
+}
