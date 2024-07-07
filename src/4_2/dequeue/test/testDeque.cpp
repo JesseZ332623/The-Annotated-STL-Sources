22479760aee7c0d4beaefe1b49a133fa91b2225f
Jesse <3191955858@qq.com>
2024-07-07 18:11:16 +0800

    双端队列容器的测试用例

--- src/4_2/dequeue/test/testDeque.cpp
+++ src/4_2/dequeue/test/testDeque.cpp
@@ -1,14 +1,35 @@
 #include "../include/deque.h"
 
+#include <algorithm>
 #include <MyLib/myLogerDef.h>
+
+//#include <deque>
 
 int main(int argc, char const *argv[])
 {
     using namespace MyLib::MyLoger;
 
-    My_Deque<int> deque_1(64, 0);
+    system("cls");
 
-    
+    My_Deque<int> deque_1;
+
+
+    for (int index = 0; index < 10; ++index)
+    {
+        deque_1.push_back(67);
+        deque_1.push_front(817);
+    }
+
+#if true
+    NOTIFY_LOG("size of deque_1 = [" + std::to_string(deque_1.size()) + "]\n");
+
+    for (const int n : deque_1)
+    {
+        std::cout << n << " ";
+    }
+#endif
+
+    DONE
 
     return EXIT_SUCCESS;
 }
