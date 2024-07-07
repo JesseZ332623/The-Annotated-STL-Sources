b009820cd6ab38b0a8721342b85b38bf2b0eaaa2
Jesse <3191955858@qq.com>
2024-07-07 18:12:14 +0800

    一个封装了 STL 标准分配器接口的类，实现基本的内存分配于释放

--- src/4_2/simple_allocator/simpleAlloc.h
+++ src/4_2/simple_allocator/simpleAlloc.h
@@ -6,33 +6,33 @@
 template <typename Type, typename Alloc>
 class Simple_Alloc
 {
-public:
-    static Type *allocate(std::size_t __n)
-    {
-        Alloc allocInstance;
-        return (!__n) ? nullptr : (Type *)allocInstance.allocate(__n * sizeof(Type));
-    }
-
-    static Type *allocate(void)
-    {
-        Alloc allocInstance;
-        return (Type *)allocInstance.allocate(sizeof(Type));
-    }
-
-    static void deallocate(Type *__ptr, std::size_t __n)
-    {
-        Alloc allocInstance;
-        if (__n != 0)
+    public:
+        static Type * allocate(std::size_t __n)
         {
-            allocInstance.deallocate(__ptr, __n * sizeof(Type));
+            Alloc allocInstance;
+            return (!__n) ? nullptr : (Type *)allocInstance.allocate(__n * sizeof(Type));
         }
-    }
 
-    static void deallocate(Type *__ptr)
-    {
-        Alloc allocInstance;
-        allocInstance.deallocate(__ptr, sizeof(Type));
-    }
+        static Type * allocate(void)
+        {
+            Alloc allocInstance;
+            return (Type *)allocInstance.allocate(sizeof(Type));
+        }
+
+        static void deallocate(Type * __ptr, std::size_t __n)
+        {
+            Alloc allocInstance;
+            if (__n != 0)
+            {
+                allocInstance.deallocate(__ptr, __n * sizeof(Type));
+            }
+        }
+
+        static void deallocate(Type *__ptr)
+        {
+            Alloc allocInstance;
+            allocInstance.deallocate(__ptr, sizeof(Type));
+        }
 };
 
 #endif // _SIMPLE_ALLOC_H_
