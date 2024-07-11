90911e329ef0827a537af57c91724a2cd1f4cb8f
Jesse <3191955858@qq.com>
2024-07-11 20:42:48 +0800

    双端队列容器的测试用例

--- src/4_2/dequeue/test/testDeque.cpp
+++ src/4_2/dequeue/test/testDeque.cpp
@@ -4,6 +4,7 @@
 #include <MyLib/myLogerDef.h>
 #include <MyLib/simpleContainerOperator.h>
 #include <MyLib/myDelay.h>
+#include <random>
 #include <deque>
 
 #ifndef DEQUE_BUFFER_SIZE
@@ -13,7 +14,7 @@
 using namespace MyLib::MyLoger;
 
 #define SHOW_DEQUE(deque)     showContainerToStream(std::cout, deque, deque.size());
-#define GET_DEQUE_SIZE(dequeName) CORRECT_LOG(std::string(dequeName) + " size = " + std::to_string(deque_1.size()) + '\n');
+#define GET_DEQUE_SIZE(deque, dequeName) CORRECT_LOG(std::string(dequeName) + " size = " + std::to_string(deque.size()) + '\n');
 
 int main(int argc, char const *argv[])
 {
@@ -21,8 +22,11 @@ int main(int argc, char const *argv[])
 
     system("cls");
 
-    
-    My_Deque<int, DEQUE_BUFFER_SIZE> deque_1 = {0x1F, 0x2D, 0x3C};
+    std::random_device defaultRandDevice;
+    std::mt19937_64 randEngine(defaultRandDevice());
+    std::uniform_int_distribution dist(0, 114514);
+
+    My_Deque<int, DEQUE_BUFFER_SIZE> deque_1 = {};
     std::size_t deque_1_bufferSize = My_Deque<int, DEQUE_BUFFER_SIZE>::iterator::getBufferSize();
 
     WARNING_LOG(
@@ -33,10 +37,30 @@ int main(int argc, char const *argv[])
     SHOW_DEQUE(deque_1);
 
 #if true
+    for (int index = 0; index < 100000000; ++index) 
+    { 
+        deque_1.push_front(dist(randEngine));
+        deque_1.push_back(dist(randEngine)); 
+        //deque_1.insert(deque_1.begin() + 5, dist(randEngine));
+        deque_1.push_front(dist(randEngine));
+        deque_1.push_back(dist(randEngine)); 
+        //deque_1.erase(deque_1.begin(), deque_1.begin() + 2);
+       
+        if (index % 1000000 == 0 && index >= 1000000) 
+        {
+            GET_DEQUE_SIZE(deque_1, "deque_1");
+        }
+    }
+#endif
 
-    NOTIFY_LOG("Call push_back() member method 12 times.\n");
+#if true
+    NOTIFY_LOG("Call clear()\n");
+    deque_1.clear();
+    GET_DEQUE_SIZE(deque_1, "deque_1");
+    NOTIFY_LOG("Call push_back(index) and push_front(index) member method 12 times.\n");
     for (int index = 0; index < 12; ++index)
     {
+        deque_1.push_front(index);
         deque_1.push_back(index);
     }
 
@@ -52,7 +76,17 @@ int main(int argc, char const *argv[])
 
     SHOW_DEQUE(deque_1);
 
-    GET_DEQUE_SIZE("deque_1");
+    NOTIFY_LOG(
+                "deque_1.at(1) = " + std::to_string(deque_1.at(1)) +
+                "\ndeque_1[5] = " + std::to_string(deque_1[5]) + "\n\n"
+            );
+
+    My_Deque<int, DEQUE_BUFFER_SIZE> deque_2(deque_1);
+    SHOW_DEQUE(deque_2);
+
+    My_Deque<int, DEQUE_BUFFER_SIZE> deque_3(std::move(deque_2));
+    SHOW_DEQUE(deque_2);
+    SHOW_DEQUE(deque_3);
 #endif
 
     DONE
