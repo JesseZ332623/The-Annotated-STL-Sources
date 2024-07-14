0a49968592c6a407cc4889b8c9dd00f54c5572e7
Jesse <3191955858@qq.com>
2024-07-14 18:05:48 +0800

    栈的测试用例

--- src/4_2/stack/test/testStack.cpp
+++ src/4_2/stack/test/testStack.cpp
@@ -1,13 +1,15 @@
 #include <iostream>
+#include <list>
 
 #include "../include/stack.h"
 
 int main(int argc, char const *argv[])
 {
     stack<int> stack_1;
+    stack<int, std::list<int>> stack_2;     // 也可以使用 std::list<int> 作为栈的底层容器。
     int index;
     
-    for (index = 0; index < 1000; ++index)
+    for (index = 0; index < 10; ++index)
     {
         stack_1.push(index);
     }
@@ -16,6 +18,17 @@ int main(int argc, char const *argv[])
     {
         std::cout << stack_1.top() << ' ';
         stack_1.pop();
+    }
+
+    for (index = 0; index < 10; ++index)
+    {
+        stack_2.push(index);
+    }
+
+    while (!stack_2.empty())
+    {
+        std::cout << stack_2.top() << ' ';
+        stack_2.pop();
     }
 
     return EXIT_SUCCESS;
