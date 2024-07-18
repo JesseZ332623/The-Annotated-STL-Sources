1ec8f5381f32aa8a9af849d2cd5b4f93593ab853
浩进 朱 <3191955858@qq.com>
2024-07-18 22:36:21 +0800

    heap 的测试用例（2024.7.18）

--- src/4_2/heap/test/testHeap.cpp
+++ src/4_2/heap/test/testHeap.cpp
@@ -1,7 +1,30 @@
 #include "../include/heap_algorithm.h"
+//#include "../../vector/include/myVector.h"
+
+#include <MyLib/myLogerDef.h>
+#include <MyLib/simpleContainerOperator.h>
+#include <vector>
 
 int main(int argc, char const *argv[])
 {
-    
-    return 0;
+    using namespace MyLib::MyLoger;
+    using namespace MyLib::SimpleContainerOperator;
+
+    std::vector<int> container = {0, 1, 2, 3, 4, 8, 9, 3, 5, 4351, 764, 3324};
+
+    showContainerToStream(std::cout, container, container.size());
+
+    std::make_heap(container.begin(), container.end());
+
+    showContainerToStream(std::cout, container, container.size());
+
+    std::pop_heap(container.begin(), container.end());
+
+    showContainerToStream(std::cout, container, container.size());
+
+    std::sort_heap(container.begin(), container.end());
+
+    showContainerToStream(std::cout, container, container.size());
+
+    return EXIT_SUCCESS;
 }
