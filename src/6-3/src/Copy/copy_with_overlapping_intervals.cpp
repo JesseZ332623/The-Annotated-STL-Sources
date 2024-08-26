d19005a69bdb331b3f22e5ea8038deba407a3e01
Jesse <3191955858@qq.com>
2024-08-26 20:55:02 +0800

    有重叠范围拷贝操作讨论

--- src/6-3/src/Copy/copy_with_overlapping_intervals.cpp
+++ src/6-3/src/Copy/copy_with_overlapping_intervals.cpp
@@ -4,6 +4,7 @@
 #include <numeric>
 #include <stdexcept>
 #include <cstring>
+#include <deque>
 
 /**
  * @brief STL 的 copy() 算法在拷贝区间是否有重叠的情况下，结果可能会出现问题，
@@ -77,6 +78,51 @@ int main(int argc, char const *argv[])
     resultCopyEnd = std::copy(array, array + 5, array + 1);
     showContain(array, array + 10, "array after overlapping intervals copy:");
 #endif
-    
+
+    /**
+     * @brief 不过，有些情况下的区间重叠的拷贝会出现错误，
+     *        如下面的 deque 所示：
+    */
+    std::deque<int> numberDeque = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
+    std::deque<int> numberDequeCopy(numberDeque.size());
+
+    /**
+     * @brief 声明两个 deuque 的迭代器，划定拷贝范围，和输出范围，如下所示：
+     * 
+     *                first  result     last
+     *        {10, 9, [8, 7, <6, 5, 4, 3), 2, 1>}
+     * 
+     *        拷贝后结果为：
+     *      
+     *        {10, 9, 8, 7, 8, 7, 6, 5, 4, 1}
+     * 
+     *         显然，这是不符合预期结果的，原因很简单，deque 所用的迭代器是 RandomAccessIterator，
+     *         这样的迭代器传入 copy() 算法中不可以执行非常底层的内存拷贝操作，所以就不会调用 memmove()，
+     *         而是非常忠实的更具传入的区间进行拷贝操作。
+    */
+    std::deque<int>::iterator copyStart     = numberDeque.begin();
+    std::deque<int>::iterator copyEnd       = numberDeque.begin();
+    std::deque<int>::iterator copyResult    = numberDeque.begin();
+    std::advance(copyStart, 2);
+    std::advance(copyEnd, 7);
+    std::advance(copyResult, 4);
+
+    NOTIFY_LOG("Original numberDeque, size = " + std::to_string(numberDeque.size()) + '\n');
+    showContainerToStream(std::cout, numberDeque, numberDeque.size());
+    printSplitLine(30, '-');
+
+    // 拷贝区间不重叠的话，自然是没有问题的
+    std::copy(copyStart, copyEnd, numberDequeCopy.begin());
+    NOTIFY_LOG("Original numberDequeCopy, size = " + std::to_string(numberDeque.size()) + '\n');
+    showContainerToStream(std::cout, numberDequeCopy, numberDequeCopy.size());
+    printSplitLine(30, '-');
+
+    // 带有重复区间的拷贝，结果会有问题。
+    std::copy(copyStart, copyEnd, copyResult);
+
+    NOTIFY_LOG("After std::copy(copyStart, copyEnd, numberDequeCopy.begin()), numberDequeCopy = \n");
+    showContainerToStream(std::cout, numberDeque, numberDeque.size());
+    printSplitLine(30, '-');
+
     return EXIT_SUCCESS;
 }
